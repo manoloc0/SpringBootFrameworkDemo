91bbe111c9ace3422e0ac1cdb738360cbb543e80
Tyler Cadenas <tmc7nrv@virginia.edu>
2026-08-14 12:02:32 -0400

    Video timestamp - 26:05 | No such bean definition available

--- src/main/java/com/tylercadenas/Doctor.java
+++ src/main/java/com/tylercadenas/Doctor.java
@@ -1,11 +1,17 @@
 package com.tylercadenas;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Doctor {
 
-    Qualification qualification; // Tightly coupled -- If you have a doctor, you NEED a qualification. What's better: loose coupling; we shouldn't rely on Qualification
+    // Beans are loaded into the container.
+    //Qualification qualification; // Tightly coupled -- If you have a doctor, you NEED a qualification. What's better: loose coupling; we shouldn't rely on Qualification
     // Spring is like an orchestrator: Spring's job will be to use the Reference graph to take care of instantiation of objects. It's like an orchestrator. (Inversion of Control)
 
     public void assist(){
+
+
         System.out.println("Doctor is assisting");
     }
 }
--- src/main/java/com/tylercadenas/Main.java
+++ src/main/java/com/tylercadenas/Main.java
@@ -1,10 +1,17 @@
 package com.tylercadenas;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
+
+        // Application Context extends Bean factory; there's more features to Application Context
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
     }
 }
