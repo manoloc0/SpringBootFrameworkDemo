63b19080e414ec007f6bf2d2b137754b84bc110e
Tyler Cadenas <tmc7nrv@virginia.edu>
2026-08-14 15:08:10 -0400

    Video timestamp - 35:04 | Constructor injection for Doctor

--- src/main/java/com/tylercadenas/Doctor.java
+++ src/main/java/com/tylercadenas/Doctor.java
@@ -3,6 +3,10 @@ package com.tylercadenas;
 public class Doctor implements Staff {
     private String qualification;
 
+    public Doctor(String qualification){
+        this.qualification = qualification;
+    }
+
     public void assist(){
         System.out.println("Doctor is assisting");
     }
