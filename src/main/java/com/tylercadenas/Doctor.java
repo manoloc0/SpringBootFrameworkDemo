2fe4ccca3e846c3c7ed196ee6cb7cdf6c5898b4a
Tyler Cadenas <tmc7nrv@virginia.edu>
2026-08-14 16:25:36 -0400

    Video timestamp - 54:20 | PostConstruct call

--- src/main/java/com/tylercadenas/Doctor.java
+++ src/main/java/com/tylercadenas/Doctor.java
@@ -1,11 +1,14 @@
 package com.tylercadenas;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 @Scope(scopeName = "singleton")
-public class Doctor implements Staff {
+public class Doctor implements Staff, BeanNameAware {
     private String qualification;
 
     public void assist(){
@@ -25,4 +28,13 @@ public class Doctor implements Staff {
         this.qualification = qualification;
     }
 
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Set bean name is called.");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Post Construct Method is called.");
+    }
 }
